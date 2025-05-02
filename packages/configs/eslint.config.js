e6975aeb9617f69a678db20d425623d943f1a978
Hugo van Rijswijk <git@hugovr.nl>
2025-05-02 17:18:06 +0200

    Add shared linting configuration

--- packages/configs/eslint.config.js
+++ packages/configs/eslint.config.js
@@ -1,56 +1,14 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import { flatConfigs } from 'eslint-plugin-import-x';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
-import { config, configs } from 'typescript-eslint';
+import { config } from 'typescript-eslint';
 
-export default config(
-  eslint.configs.recommended,
-  ...configs.recommendedTypeChecked,
-  ...configs.stylisticTypeChecked,
-  flatConfigs.recommended,
-  flatConfigs.typescript,
-  {
-    languageOptions: {
-      parserOptions: {
-        project: true,
-      },
-      globals: {
-        ...globals.node,
-      },
-    },
-    linterOptions: {
-      reportUnusedDisableDirectives: 'error',
-    },
-    rules: {
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/no-import-type-side-effects': 'error',
-      'import-x/newline-after-import': 'error',
-      'import-x/no-deprecated': 'error',
+import shared from '../../eslint.config.shared.js';
 
-      'simple-import-sort/exports': 'error',
-      'simple-import-sort/imports': 'error',
-
-      eqeqeq: 'error',
-    },
-    plugins: {
-      'simple-import-sort': simpleImportSort,
+export default config(...shared, {
+  languageOptions: {
+    globals: {
+      ...globals.node,
     },
   },
-  {
-    // Test-specific rules
-    files: ['src/integration/**/*.ts', 'src/**/*.test.ts', 'src/**/*.spec.ts'],
-    rules: {
-      '@typescript-eslint/no-unused-expressions': 'off',
-    },
-  },
-  {
-    files: ['*.js', '*.config.{js,ts}', 'tasks/**/*.{js,ts}', '.husky/**/*.{js,ts}'],
-    ...configs.disableTypeChecked,
-  },
-  {
-    ignores: ['node_modules', 'dist', 'public', 'reports', 'src/graphql/gen', 'gen'],
-  },
-);
+});
---
+++ packages/schema/eslint.config.js
@@ -0,0 +1,7 @@
+// @ts-check
+
+import { config } from 'typescript-eslint';
+
+import shared from '../../eslint.config.shared.js';
+
+export default config(...shared);
