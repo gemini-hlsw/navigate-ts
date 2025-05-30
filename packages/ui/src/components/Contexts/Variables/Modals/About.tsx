a668a61a2792ca3aab8feeda8781aec7997e46cd
Diego Gomez <diego.gomez@noirlab.edu>
2025-05-30 11:39:38 -0400

    Add about information

--- packages/ui/src/components/Contexts/Variables/Modals/About.tsx
+++ packages/ui/src/components/Contexts/Variables/Modals/About.tsx
@@ -5,20 +5,45 @@ import { useCallback } from 'react';
 
 import { useAboutVisible } from '@/components/atoms/about';
 import { frontendVersion } from '@/Helpers/constants';
+import { environment } from '@/Helpers/environment';
 
 export function About() {
   const [aboutVisible, toggleAboutVisible] = useAboutVisible();
+  const odbHost = new URL(environment.odbURI).hostname.split('.')[0];
 
   const onHide = useCallback(() => toggleAboutVisible(false), [toggleAboutVisible]);
   const configsVersion = useConfigsVersion().data?.version.version;
   const serverVersion = useServerVersion().data?.serverVersion;
 
   return (
-    <Dialog header="NAVIGATE" visible={aboutVisible} modal onHide={onHide}>
+    <Dialog header="About Navigate" visible={aboutVisible} modal onHide={onHide}>
       <div className="about-dialog">
-        <p>Frontend: {frontendVersion}</p>
-        <p>Configs API: {configsVersion}</p>
-        <p>Server: {serverVersion}</p>
+        <table>
+          <tbody>
+            <tr>
+              <td>Environment</td>
+              <td>{environment.environment}</td>
+            </tr>
+            <tr>
+              <td>ODB</td>
+              <td>
+                {odbHost.includes('staging') ? 'staging' : odbHost.includes('dev') ? 'development' : 'production'}
+              </td>
+            </tr>
+            <tr>
+              <td>Frontend</td>
+              <td>{frontendVersion}</td>
+            </tr>
+            <tr>
+              <td>Configs API</td>
+              <td>{configsVersion}</td>
+            </tr>
+            <tr>
+              <td>Server</td>
+              <td>{serverVersion}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </Dialog>
   );
