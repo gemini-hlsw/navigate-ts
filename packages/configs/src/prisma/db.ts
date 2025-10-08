a510d236c280a18a0ca9f4ff47eb9835b0cfaa89
Hugo van Rijswijk <git@hugovr.nl>
2025-10-08 11:56:54 +0200

    Use Prisma without Rust engine

--- packages/configs/src/prisma/db.ts
+++ packages/configs/src/prisma/db.ts
@@ -1,6 +1,10 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+
 import { extendPrisma } from './extend.ts';
 import { PrismaClient as GenPrismaClient } from './gen/client.ts';
 
-export const prisma = extendPrisma(new GenPrismaClient());
+export const prisma = extendPrisma(
+  new GenPrismaClient({ adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }) }),
+);
 
 export type PrismaClient = typeof prisma;
