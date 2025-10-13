540fb31f79b0bb45b9e6b025f47b42cb0e7b5f95
Hugo van Rijswijk <git@hugovr.nl>
2025-10-13 10:32:15 +0200

    Set rejectUnauthorized to false
    Needed for heroku

--- packages/configs/src/prisma/db.ts
+++ packages/configs/src/prisma/db.ts
@@ -4,7 +4,9 @@ import { extendPrisma } from './extend.ts';
 import { PrismaClient as GenPrismaClient } from './gen/client.ts';
 
 export const prisma = extendPrisma(
-  new GenPrismaClient({ adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }) }),
+  new GenPrismaClient({
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }),
+  }),
 );
 
 export type PrismaClient = typeof prisma;
