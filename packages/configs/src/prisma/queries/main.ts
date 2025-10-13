7eae3e70c395eadb61cc20eee4a8b05d9b113c19
Hugo van Rijswijk <git@hugovr.nl>
2025-10-13 17:34:08 +0200

    Read migration files instead of executing prisma migrate cli (#1079)

--- packages/configs/src/prisma/queries/main.ts
+++ packages/configs/src/prisma/queries/main.ts
@@ -1,6 +1,6 @@
 import type { PrismaClient } from '../db.ts';
 import { write } from './write.ts';
 
-export async function populateDb(client: PrismaClient) {
-  await write(client);
+export async function populateDb(client: PrismaClient, log = console.log) {
+  await write(client, log);
 }
