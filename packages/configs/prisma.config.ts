438f269f36bfaff6321f5a4f24838c3dadfa21b2
Hugo van Rijswijk <git@hugovr.nl>
2025-11-24 12:02:23 +0100

    Update prisma to major version 7 and adjust configuration accordingly

--- packages/configs/prisma.config.ts
+++ packages/configs/prisma.config.ts
@@ -5,7 +5,14 @@ import { defineConfig } from 'prisma/config';
 
 if (fs.existsSync('./.env')) process.loadEnvFile('./.env');
 
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL environment variable is not set.');
+}
+
 export default defineConfig({
+  datasource: { url: connectionString },
   schema: path.join('prisma', 'schema.prisma'),
   migrations: {
     path: path.join('prisma', 'migrations'),
